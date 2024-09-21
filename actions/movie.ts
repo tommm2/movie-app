caa3462bde34a6d6c5220ea48e4cea6a657427d1
tommm2 <tom875694231@gmail.com>
2024-09-21 23:53:25 +0800

    feat: add movie actions

---
+++ actions/movie.ts
@@ -0,0 +1,19 @@
+'use server';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+export const getMovies = async (page = 1, limit = 10) => {
+	try {
+		const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`;
+		const res = await fetch(url);
+
+		const { results } = await res.json();
+
+		const resultsWithLimit = results.slice(0, limit);
+
+		return resultsWithLimit;
+	} catch (error) {
+		throw new Error(`An error happened: ${error}`)
+	}
+}
