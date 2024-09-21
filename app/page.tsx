2b947009342ba1a9317da72f66fbe85c97227364
tommm2 <tom875694231@gmail.com>
2024-09-21 23:55:43 +0800

    chore: update root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-  return (
-    <main className="">
-      hello world
-    </main>
-  );
+import { getMovies } from '@/actions/movie';
+import MovieList from '@/components/movie-list';
+
+export default async function Home() {
+	const movies = await getMovies();
+
+	return (
+		<main className='mx-auto max-w-[90rem] p-4 pt-8'>
+			<h1 className='mb-4 text-2xl font-bold'>Discover your Movie</h1>
+			<MovieList initialMovies={movies} />
+		</main>
+	);
 }
