dacf1ba6e620ad24a953207f84c5bb08b547e72d
tommm2 <tom875694231@gmail.com>
2024-09-23 20:59:16 +0800

    chore: update movie page

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default async function Home() {
 	const movies = await getMovies();
 
 	return (
-		<main className='mx-auto max-w-[90rem] p-4 pt-8'>
-			<h1 className='mb-4 text-2xl font-bold'>Discover your Movie</h1>
+		<>
+			<h1 className='mb-4 text-2xl font-bold'>Movies</h1>
 			<MovieList initialMovies={movies} />
-		</main>
+		</>
 	);
 }
