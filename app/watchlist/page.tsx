65e8be82fb867750f3324d016a9ac5c08eb45383
tommm2 <tom875694231@gmail.com>
2024-09-23 21:01:50 +0800

    feat: get watchlist data in page

--- app/watchlist/page.tsx
+++ app/watchlist/page.tsx
@@ -1,9 +1,50 @@
+'use client';
+
+import { collection, getDocs } from 'firebase/firestore';
+import { useCallback, useEffect, useState } from 'react';
+
+import MovieCard from '@/components/movie-card';
 import ProtectedRoute from '@/components/protected-route';
+import { useAuth } from '@/context/auth-context';
+import { db } from '@/lib/firebase';
 
 export default function Watchlist() {
+	const { user } = useAuth();
+	const [watchlist, setWatchlist] = useState([]);
+
+	const getWatchlist = useCallback(async () => {
+		if (user) {
+			const querySnapshot = await getDocs(
+				collection(db, 'users', user.uid as string, 'watchlist'),
+			);
+
+			const data = querySnapshot.docs.map((doc) => ({
+				...doc.data(),
+			}));
+
+			setWatchlist(data as any);
+		}
+	}, [user]);
+
+	useEffect(() => {
+		getWatchlist();
+	}, [getWatchlist]);
+
 	return (
 		<ProtectedRoute>
-			<main>watchlist</main>
+			<h1 className='mb-4 text-2xl font-bold'>Watchlist</h1>
+			<div className='grid grid-cols-1 gap-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4'>
+				{watchlist.map((item: any) => {
+					return (
+						<MovieCard
+							key={item.id}
+							id={item.id}
+							title={item.title}
+							posterPath={item.posterPath}
+						/>
+					);
+				})}
+			</div>
 		</ProtectedRoute>
 	);
 }
