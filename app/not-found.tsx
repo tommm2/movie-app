9b270057a56bfeb720678805882dd3031aa25c75
tommm2 <tom875694231@gmail.com>
2024-09-24 22:58:28 +0800

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+	return (
+		<main className='mx-auto max-w-[90rem] p-4 pt-8'>
+			<h1 className='mb-4 text-3xl text-center font-bold'>Opps... Page Not Found</h1>
+		</main>
+	);
+}
