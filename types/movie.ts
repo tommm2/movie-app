9b65d0351654598f4ef98e447db4f11024d94eb4
tommm2 <tom875694231@gmail.com>
2024-09-24 22:49:51 +0800

    update movie types

--- types/movie.ts
+++ types/movie.ts
@@ -1,40 +1,103 @@
 export interface Movie {
-	adult: boolean;
-	backdrop_path: string;
-	genre_ids: number[];
-	id: number;
-	original_language: string;
-	original_title: string;
-	overview: string;
-	popularity: number;
-	poster_path: string;
-	release_date: string;
-	title: string;
-	video: boolean;
-	vote_average: number;
-	vote_count: number;
+	adult: boolean
+	backdrop_path: string
+	genre_ids: number[]
+	id: number
+	original_language: string
+	original_title: string
+	overview: string
+	popularity: number
+	poster_path: string
+	release_date: string
+	title: string
+	video: boolean
+	vote_average: number
+	vote_count: number
 }
 
-export interface Credits {
-	cast: Credit[];
-	crew: Credit[];
+export interface MovieDetail {
+	adult: boolean
+	backdrop_path: string
+	belongs_to_collection: any
+	budget: number
+	genres: Genre[]
+	homepage: string
+	id: number
+	imdb_id: string
+	origin_country: string[]
+	original_language: string
+	original_title: string
+	overview: string
+	popularity: number
+	poster_path: string
+	production_companies: ProductionCompany[]
+	production_countries: ProductionCountry[]
+	release_date: string
+	revenue: number
+	runtime: number
+	spoken_languages: SpokenLanguage[]
+	status: string
+	tagline: string
+	title: string
+	video: boolean
+	vote_average: number
+	vote_count: number
+	credits: Credits
 }
 
 export interface Genre {
-	id: number;
-	name: string;
+	id: number
+	name: string
 }
 
-export interface Credit {
-	adult: boolean;
-	gender: number;
-	id: number;
-	known_for_department: string;
-	name: string;
-	original_name: string;
-	popularity: number;
-	profile_path: string;
-	credit_id: string;
-	department: string;
+export interface ProductionCompany {
+	id: number
+	logo_path: string
+	name: string
+	origin_country: string
+}
+
+export interface ProductionCountry {
+	iso_3166_1: string
+	name: string
+}
+
+export interface SpokenLanguage {
+	english_name: string
+	iso_639_1: string
+	name: string
+}
+
+export interface Credits {
+	cast: Cast[]
+	crew: Crew[]
+}
+
+export interface Cast {
+	adult: boolean
+	gender: number
+	id: number
+	known_for_department: string
+	name: string
+	original_name: string
+	popularity: number
+	profile_path: string
+	cast_id: number
+	character: string
+	credit_id: string
+	order: number
+}
+
+export interface Crew {
+	adult: boolean
+	gender: number
+	id: number
+	known_for_department: string
+	name: string
+	original_name: string
+	popularity: number
+	profile_path: string
+	credit_id: string
+	department: string
 	job: string
 }
