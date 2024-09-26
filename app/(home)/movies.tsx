c09b29024ed95fda37f62d39f7d22cf864ed8868
tommm2 <tom875694231@gmail.com>
2024-09-26 08:52:31 +0800

    update home page movie section

--- app/(home)/movies.tsx
+++ app/(home)/movies.tsx
@@ -1,40 +1,59 @@
 'use client';
 
 import { Loader2 } from 'lucide-react';
-import { useCallback, useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useEffect, useMemo, useState } from 'react';
 
 import { getMovies } from '@/actions/movie';
 import MovieCard from '@/components/movie-card';
+import SortControl from '@/components/sort-control';
+import { useIntersection } from '@/hooks/use-intersection';
+import { sortMovies } from '@/lib/utils';
 import { type Movie } from '@/types/movie';
+import type { OrderBy, SortBy } from '@/types/sort';
 
 interface MoviesProps {
 	initialMovies: Movie[];
 }
 
 export default function Movies({ initialMovies }: MoviesProps) {
-	const { ref, inView } = useInView();
+	const { ref, isIntersecting } = useIntersection();
 
 	const [movies, setMovies] = useState(initialMovies);
+	const [orderBy, setOrderBy] = useState<OrderBy>('');
+	const [sortBy, setSortBy] = useState<SortBy>('desc');
 	const [page, setPage] = useState(2);
 
-	const loadMore = useCallback(async () => {
-		const results = await getMovies(page);
-
-		setMovies((prevMovies) => [...prevMovies, ...results]);
-		setPage((prevPage) => prevPage + 1);
-	}, [page]);
-
 	useEffect(() => {
-		if (inView) {
-			loadMore();
+		if (isIntersecting) {
+			getMovies(page).then((data) => {
+				setMovies((prevMovies) => {
+					const movieIdSet = new Set(prevMovies.map((movie) => movie.id));
+					const filteredNewMovies = data.filter(
+						(movie: Movie) => !movieIdSet.has(movie.id),
+					);
+
+					return [...prevMovies, ...filteredNewMovies];
+				});
+				setPage((prevPage) => prevPage + 1);
+			});
 		}
-	}, [inView, loadMore]);
+	}, [isIntersecting, page]);
+
+	const sortedMovies = useMemo(() => {
+		return sortMovies({ orderBy, sortBy, data: movies });
+	}, [orderBy, sortBy, movies]);
 
 	return (
 		<>
+			<SortControl
+				orderBy={orderBy}
+				sortBy={sortBy}
+				onOrderBy={setOrderBy}
+				onSortBy={setSortBy}
+			/>
+
 			<div className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-				{movies.map((item) => {
+				{sortedMovies.map((item) => {
 					return (
 						<MovieCard
 							key={item.id}
@@ -44,11 +63,11 @@ export default function Movies({ initialMovies }: MoviesProps) {
 				})}
 			</div>
 
-			<div className='flex w-full justify-center'>
-				<Loader2
-					ref={ref}
-					className='size-8 animate-spin'
-				/>
+			<div
+				ref={ref}
+				className='flex w-full justify-center'
+			>
+				<Loader2 className='size-8 animate-spin' />
 			</div>
 		</>
 	);
