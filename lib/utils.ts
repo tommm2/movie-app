96a7b15a2637c17fe1963d389b419b6c485c4632
tommm2 <tom875694231@gmail.com>
2024-09-26 08:53:13 +0800

    feat: add sortMovie util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
+import { Movie } from "@/types/movie";
+import type { OrderBy, SortBy } from "@/types/sort";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+export function sortMovies({
+	orderBy,
+	sortBy,
+	data,
+}: {
+	orderBy: OrderBy;
+	sortBy: SortBy;
+	data: Movie[];
+}) {
+	if (!orderBy) return data;
+
+	return data.sort((a: any, b: any) => {
+		return sortBy === 'desc'
+			? b[orderBy] - a[orderBy]
+			: a[orderBy] - b[orderBy];
+	});
 }
