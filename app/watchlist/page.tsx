f036afd0a0d8a3ef31a41fc78fcdee617a32d9b5
tommm2 <tom875694231@gmail.com>
2024-09-26 08:52:44 +0800

    update watchlist page

--- app/watchlist/page.tsx
+++ app/watchlist/page.tsx
@@ -1,35 +1,38 @@
 'use client';
 
 import { collection, getDocs } from 'firebase/firestore';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import MovieCard from '@/components/movie-card';
 import ProtectedRoute from '@/components/protected-route';
+import SortControl from '@/components/sort-control';
 import { useAuth } from '@/context/auth-context';
 import { db } from '@/lib/firebase';
+import { sortMovies } from '@/lib/utils';
 import { Movie } from '@/types/movie';
+import type { OrderBy, SortBy } from '@/types/sort';
 
 export default function Watchlist() {
 	const { user } = useAuth();
+	const [orderBy, setOrderBy] = useState<OrderBy>('');
+	const [sortBy, setSortBy] = useState<SortBy>('desc');
 	const [watchlist, setWatchlist] = useState([]);
 
-	const getWatchlist = useCallback(async () => {
-		if (user) {
-			const querySnapshot = await getDocs(
-				collection(db, 'users', user.uid as string, 'watchlist'),
-			);
-
-			const data = querySnapshot.docs.map((doc) => ({
-				...doc.data(),
-			}));
-
-			setWatchlist(data as any);
-		}
-	}, [user]);
+	const sortedMovies = sortMovies({ orderBy, sortBy, data: watchlist });
 
 	useEffect(() => {
-		getWatchlist();
-	}, [getWatchlist]);
+		if (user) {
+			getDocs(collection(db, 'users', user.uid as string, 'watchlist')).then(
+				(querySnapshot) => {
+					const data = querySnapshot.docs.map((doc) => ({
+						...doc.data(),
+					}));
+
+					setWatchlist(data as any);
+				},
+			);
+		}
+	}, [user]);
 
 	return (
 		<ProtectedRoute>
@@ -38,9 +41,16 @@ export default function Watchlist() {
 					Watchlist
 				</h1>
 
+				<SortControl
+					orderBy={orderBy}
+					sortBy={sortBy}
+					onOrderBy={setOrderBy}
+					onSortBy={setSortBy}
+				/>
+
 				{watchlist.length > 0 ? (
 					<div className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-						{watchlist.map((item: Movie) => {
+						{sortedMovies.map((item: Movie) => {
 							return (
 								<MovieCard
 									key={item.id}
