877d9ea9debc19cfe27c0c55643b18240d7c8e38
tommm2 <tom875694231@gmail.com>
2024-09-26 13:35:17 +0800

    update watchlist page

--- app/watchlist/page.tsx
+++ app/watchlist/page.tsx
@@ -3,6 +3,7 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
+import LotteryModal from '@/components/lottery-modal';
 import MovieCard from '@/components/movie-card';
 import ProtectedRoute from '@/components/protected-route';
 import SortControl from '@/components/sort-control';
@@ -16,20 +17,21 @@ export default function Watchlist() {
 	const { user } = useAuth();
 	const [orderBy, setOrderBy] = useState<OrderBy>('');
 	const [sortBy, setSortBy] = useState<SortBy>('desc');
-	const [watchlist, setWatchlist] = useState([]);
+	const [watchlist, setWatchlist] = useState<Movie[]>([]);
 
 	const sortedMovies = sortMovies({ orderBy, sortBy, data: watchlist });
 
 	useEffect(() => {
 		if (user) {
-			getDocs(collection(db, 'users', user.uid as string, 'watchlist'))
-				.then((querySnapshot) => {
+			getDocs(collection(db, 'users', user.uid as string, 'watchlist')).then(
+				(querySnapshot) => {
 					const data = querySnapshot.docs.map((doc) => ({
 						...doc.data(),
 					}));
 
-					setWatchlist(data as any);
-				});
+					setWatchlist(data as Movie[]);
+				},
+			);
 		}
 	}, [user]);
 
@@ -40,12 +42,16 @@ export default function Watchlist() {
 					Watchlist
 				</h1>
 
-				<SortControl
-					orderBy={orderBy}
-					sortBy={sortBy}
-					onOrderBy={setOrderBy}
-					onSortBy={setSortBy}
-				/>
+				<div className='flex flex-col gap-4 sm:flex-row sm:justify-between'>
+					<LotteryModal watchlist={watchlist} />
+					<SortControl
+						className='w-full sm:w-[180px]'
+						orderBy={orderBy}
+						sortBy={sortBy}
+						onOrderBy={setOrderBy}
+						onSortBy={setSortBy}
+					/>
+				</div>
 
 				{watchlist.length > 0 ? (
 					<div className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
