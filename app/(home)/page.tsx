d667c65f4e36ac868da87abf714d3eef5aa500bd
tommm2 <tom875694231@gmail.com>
2024-09-26 09:11:29 +0800

    update home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,6 @@
-import { getMovies } from '@/actions/movie';
 import Movies from './movies';
 
-export default async function Home() {
-	const data = await getMovies();
-
+export default function Home() {
 	return (
 		<main className='mx-auto max-w-[90rem] p-4 pt-8'>
 			<h1 className='mb-4 text-3xl font-bold'>Movies</h1>
